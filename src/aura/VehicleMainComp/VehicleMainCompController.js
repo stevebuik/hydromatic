8fe563b49800e41ebc08c667e066a9d546f8f0a6
Zhiren Guan <jasong327@hotmail.com>
2015-07-22 22:31:00 +1000

    Added ApplicationMessage with VehiclePartDemo

--- src/aura/VehicleMainComp/VehicleMainCompController.js
+++ src/aura/VehicleMainComp/VehicleMainCompController.js
@@ -11,9 +11,23 @@
         var demoComponent = component.find(event.getParams().context);
         delete event.getParams().context;
         demoComponent.set("v.debugData", JSON.stringify(event.getParams()));
+	},
+    showApplicationMessage : function(component, event, helper){
         
-		var sampleSVG = 'digraph R {  rankdir=LR  node [style=rounded]  node1 [shape=box]  node2 [fillcolor=yellow, style="rounded,filled", shape=diamond]  node3 [shape=record, label="{ a | b | c }"] node1 -> node2 -> node3}';
+        var severity = component.find("inputSeverity").get("v.value");
+
+        var appEvent = $A.get("e.c:ApplicationMessageEvent");
+        appEvent.setParams({ "isShow": true,
+							 "severity": severity,
+                            "title" : "Test Title",
+                            "body" : "Test Body"
+                           })
+        		.fire();
         
+        component.find("demo4").set("v.debugData", "selected severity: " + severity);
+    },
+    showGraphvizContent : function(component, event, helper){
+        var sampleSVG = 'digraph R {  rankdir=LR  node [style=rounded]  node1 [shape=box]  node2 [fillcolor=yellow, style="rounded,filled", shape=diamond]  node3 [shape=record, label="{ a | b | c }"] node1 -> node2 -> node3}';
         component.find("vehicleRenderer").set("v.graphvizContent", sampleSVG);
-	}
+    }
 })
