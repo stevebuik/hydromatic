7bf8a1407667cc6932bc60113495da992f7cc18d
Steve Buikhuizen <steve.buikhuizen@gmail.com>
2015-08-20 13:14:51 +1000

    CHORE removed focus behaviour from VehiclePart and extend FocusableComponent instead

--- src/aura/VehiclePartDemo/VehiclePartDemoController.js
+++ src/aura/VehiclePartDemo/VehiclePartDemoController.js
@@ -1,8 +1,2 @@
 ({
-	handleFocus : function(component, event, helper) {
-        var focus = event.getParams().demoId;
-        var isInFocus = focus == null || focus == component.get("v.focusId");
-        $A.util.removeClass(component.getElement(), isInFocus?'unfocused':'focused');
-        $A.util.addClass(component.getElement(), isInFocus?'focused':'unfocused');
-	}
 })
