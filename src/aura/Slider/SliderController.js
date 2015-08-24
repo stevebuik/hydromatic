5f0a7c6928d49d78f4b45ccc5325bd4cbea646c7
Zhiren Guan <jasong327@hotmail.com>
2015-08-25 09:09:22 +1000

    Remove the main component dependency for slider component

--- src/aura/Slider/SliderController.js
+++ src/aura/Slider/SliderController.js
@@ -1,9 +1,8 @@
 ({
-    javascriptReady : function(component, event, helper) {
-        if (event.getParam("library") == "JQUERY") {
-            $j("#sliderInput").slider();
-            $j("#sliderInput").on('slide', function(slideEvt) {
-                $j("#sliderValue").text(slideEvt.value);
+    init : function(component, event, helper) {
+            $("#sliderInput").slider();
+            $("#sliderInput").on('slide', function(slideEvt) {
+                $("#sliderValue").text(slideEvt.value);
             });   
         }
 	}
